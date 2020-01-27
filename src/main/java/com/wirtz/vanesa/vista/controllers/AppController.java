e7dd838139ee103a0eb11d70859741f778178abf
vanesa <vtorresbarca@gmail.com>
2020-01-27 16:19:54 +0100

    terminada pagina de index, falta responsive

--- src/main/java/com/wirtz/vanesa/vista/controllers/AppController.java
+++ src/main/java/com/wirtz/vanesa/vista/controllers/AppController.java
@@ -5,34 +5,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppController {
-	
-	@GetMapping({"/","/login"})
+
+	@GetMapping({ "/" })
 	public String index() {
 		return "index";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
 	}
 	
 	@GetMapping("/menu")
 	public String menu() {
 		return "menu";
 	}
-	
+
 	@GetMapping("/user")
 	public String user() {
 		return "user";
 	}
-	
+
 	@GetMapping("/admin")
 	public String admin() {
 		return "admin";
 	}
-	
-	@GetMapping("/admin/soloAdmin")
-	public String soloAdmin() {
-		return "soloAdmin";
-	}
-	
-	@GetMapping("/user/soloUser")
-	public String soloUser() {
-		return "soloUser";
-	}
+
 }
